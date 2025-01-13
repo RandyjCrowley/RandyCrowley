cc33b9f1575b685dbd62aecba4ca51cc2654de57
Randy <83294978+RandyjCrowley@users.noreply.github.com>
2025-01-13 13:44:04 -0600

    Update Dockerfile and routes

--- Dockerfile
+++ Dockerfile
@@ -1,46 +1,58 @@
+###########################
+# Install JS Dependencies #
+###########################
+FROM node:18 as build
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+
+COPY package*.json ./
+COPY *.config.js ./
+COPY resources ./resources
+RUN mkdir -p /usr/src/app/public
+
+RUN npm install -D
+RUN npm run build
+
+###########################
+# Create Production Image #
+###########################
 FROM serversideup/php:8.3-fpm-apache
 
-# Switch to root for system installations
 USER root
 
-# Install Node.js and other dependencies
-RUN curl -fsSL https://deb.nodesource.com/setup_lts.x | bash - && \
-    apt-get update && \
-    apt-get install -y nodejs git ghostscript vim --no-install-recommends && \
-    apt-get clean && \
-    rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* /usr/share/doc/*
+# Install required packages
+RUN apt-get update \
+    && apt-get install -y --no-install-recommends \
+    vim \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* /usr/share/doc/*
 
 WORKDIR /var/www/html
 
-# Install composer from the official image
-COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
-
-# Install necessary PHP extensions
+# Install PHP extensions
 RUN install-php-extensions imagick gd
 
+# Install composer
+COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
 
+# Copy Apache configuration
+RUN a2enmod rewrite \
+    && a2ensite ssl-off.conf
 
-# Copy composer files and install dependencies
+# Copy JS dependencies from build stage
+COPY --from=build /usr/src/app/node_modules ./node_modules
+
+# Install composer dependencies
 COPY composer.* ./
-RUN composer install --no-autoloader --no-interaction --no-progress --prefer-dist --no-dev -o
+RUN composer install --no-autoloader --no-interaction --no-progress
 
-# Copy application code and generate autoload files
-COPY . .
-RUN composer dumpautoload -o
+# Copy application files
+COPY --chown=www-data:www-data . .
+COPY --from=build /usr/src/app/public/build ./public/build
+RUN composer dump-autoload --optimize
 
-# Install and build frontend assets
-RUN npm install && npm run build
+EXPOSE 8080
 
-# Publish assets
-RUN php artisan vendor:publish --tag=log-viewer-assets --force
-
-# Expose port 80
-EXPOSE 80
-
-RUN chown -R www-data:www-data /var/www/html
-
-# Switch to www-data user for running the application
 USER www-data
 
-# Define entrypoint
 ENTRYPOINT ["/init"]
--- routes/web.php
+++ routes/web.php
@@ -16,18 +16,20 @@ Route::get('auth/{provider}/callback', [AuthController::class, 'handleProviderCa
 Route::get('open-box/{storageBox}', [StorageBoxController::class, 'show'])->name('open-box');
 
 Route::middleware('auth')->group(function () {
-    Route::get('dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-    Route::get('redirect/{to}', [RedirectContoller::class, 'redirect'])->name('redirect');
-
-    Route::get('storage-boxes', [StorageBoxController::class, 'index'])->name('storage-boxes');
-    Route::post('storage-boxes', [StorageBoxController::class, 'store'])->name('storage-boxes.store');
-    Route::get('storage/{storageBox}', [StorageBoxController::class, 'qr'])->name('storage-qr');
-
-    Route::get('imager', [ImagerController::class, 'index'])->name('imager');
-    Route::post('imager-upload-init', [ImagerController::class, 'initializeUpload'])->name('imager-upload-init');
-    Route::post('imager-upload-chunk', [ImagerController::class, 'uploadChunk'])->name('imager-upload-chunk');
-    Route::post('imager-upload-finalize', [ImagerController::class, 'finalizeUpload'])->name('imager-upload-finalize');
-    Route::post('imager-upload-cancel', [ImagerController::class, 'cancelUpload'])->name('imager-upload-cancel');
+    if (request()->user()->id === 1) {
+        Route::get('dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+        Route::get('redirect/{to}', [RedirectContoller::class, 'redirect'])->name('redirect');
+    
+        Route::get('storage-boxes', [StorageBoxController::class, 'index'])->name('storage-boxes');
+        Route::post('storage-boxes', [StorageBoxController::class, 'store'])->name('storage-boxes.store');
+        Route::get('storage/{storageBox}', [StorageBoxController::class, 'qr'])->name('storage-qr');
+    
+        Route::get('imager', [ImagerController::class, 'index'])->name('imager');
+        Route::post('imager-upload-init', [ImagerController::class, 'initializeUpload'])->name('imager-upload-init');
+        Route::post('imager-upload-chunk', [ImagerController::class, 'uploadChunk'])->name('imager-upload-chunk');
+        Route::post('imager-upload-finalize', [ImagerController::class, 'finalizeUpload'])->name('imager-upload-finalize');
+        Route::post('imager-upload-cancel', [ImagerController::class, 'cancelUpload'])->name('imager-upload-cancel');
+    }
 });
 
 Route::get('/', function () {
